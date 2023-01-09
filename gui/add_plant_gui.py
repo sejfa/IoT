2731e22e92a69a6cd14a7edcd75065f1d3415f31
Alem Sejfic <sejficalem95@gmail.com>
2023-01-09 18:29:46 +0100

    update plant changes

--- gui/add_plant_gui.py
+++ gui/add_plant_gui.py
@@ -1,4 +1,3 @@
-from email.errors import MessageError
 import tkinter as tk
 from tkinter import ttk, messagebox, filedialog
 #from gui import list_gui
@@ -35,15 +34,9 @@ class AddPlant(tk.Frame):
         tab3 = tabview.add("Delete plant")
         tabview.set("Add plant")  # set currently visible tab
 
-    
-
-        #fetching column Plant
-        """conn = sqlite3.connect('PyFlora.db')
-        c = conn.cursor()
-        c.execute("SELECT Plant FROM RecordsOfPlants")
-        result = c.fetchall()
-        new_list = [i for i, in result]"""
         
+                    
+
         
         ################################   tab1 Add plant  #######################################
         
@@ -65,9 +58,12 @@ class AddPlant(tk.Frame):
             tab1, textvariable=add_entry_var, corner_radius=8, fg_color="#f6f6f6",text_color="black")
         add_entry.place(x=135, y=30)
 
-        
+        open_button = customtkinter.CTkButton(
+            master=tab1, width=90, text="Choose Image", height=10, compound="left", command=lambda:openFile())
+
+
         def insert_data():
-            global filepath, data
+            global filepath, data, read_file
             read_file = open(filepath,'rb')
             read_file = read_file.read()
             
@@ -75,10 +71,10 @@ class AddPlant(tk.Frame):
             try:
                 conn = sqlite3.connect('PyFlora.db')
                 c = conn.cursor()
-                data = (add_entry.get(),read_file)
+                data = (add_entry.get().capitalize(),read_file)
 
                 c.execute("SELECT Plant FROM RecordsOfPlants WHERE (Plant=?)",
-                        [add_entry.get()])
+                        [add_entry.get().capitalize()])
                 
                 result = c.fetchone()
                 
@@ -100,11 +96,40 @@ class AddPlant(tk.Frame):
                     print("Failed to add data", error)
 
 
+        save_image = customtkinter.CTkImage(Image.open(
+            "media\icons8-save-100.png").resize((20, 20), Image.ANTIALIAS))
+
+        save_button = customtkinter.CTkButton(
+            master=tab1, width=90, image=save_image, text="Save",compound="left", command=insert_data)
+
+
+
 
         ################################   tab2 Update plant  #####################################
-        
+        def changeFile():
+            global filepath2, file_label
+            filepath2 = filedialog.askopenfilename(initialdir="C:\Alem\Programiranje\python_vsc\Zavrsni_AS\media", filetypes=(('jpg', '*.jpg'),('png','*.png'),('All files','*.*'))) 
+            
+            
+            base_filename = os.path.basename(filepath2)
+            file_label2 = ttk.Label(tab2, text=base_filename, background="#f6f6f6")
+            file_label2.place(x=95, y=130)  
+
+
         small_label(tab2, "Update plant", 10, 35)
-        small_label(tab2, "Change Image", 10, 90)
+        small_label(tab2,"Select Plant Id", 10, 80)
+        small_label(tab2, "Change Image", 10, 130)
+
+        #fetching column Plant
+        conn = sqlite3.connect('PyFlora.db')
+        c = conn.cursor()
+        c.execute("SELECT Plant_id FROM RecordsOfPlants")
+        result = c.fetchall()
+        new_list = [item for i in result for item in i]
+        
+        dropmenu_var = tk.StringVar()
+        dropmenu = ttk.OptionMenu(tab2, dropmenu_var, *new_list)
+        dropmenu.place(x=220 ,y=80)    
 
         plant_var = tk.StringVar()
         update_plant_entry = customtkinter.CTkEntry(tab2, textvariable=plant_var, corner_radius=8, fg_color="#f6f6f6",text_color="black")
@@ -114,29 +139,38 @@ class AddPlant(tk.Frame):
         update_image_entry = customtkinter.CTkEntry(tab2, textvariable=update_var, corner_radius=8, fg_color="#f6f6f6",text_color="black")
         update_image_entry.place()
 
+    
         conn = sqlite3.connect('Pyflora.db')
         c = conn.cursor()
 
         def insert_updated_data():
-            global data
             
-            c.execute("SELECT * FROM RecordsOfPlants WHERE Plant=?",
-                [update_plant_entry.get()])
+            global data, filepath2
+            read_file2 = open(filepath2,'rb')
+            read_file2 = read_file2.read()
+            
+            try:
+                conn = sqlite3.connect('PyFlora.db')
+                c = conn.cursor()
+                c.execute("SELECT * FROM RecordsOfPlants WHERE Plant=?",
+                    [update_plant_entry.get().capitalize()])
 
-            result = c.fetchone()
-            
-            if result:
-                c.execute("UPDATE RecordsOfPlants set Plant=? WHERE Plant_id=?",
-                    [update_plant_entry.get()])
+                result = c.fetchone()
                 
-                conn.commit()
-
+                if result:
+                    c.execute("UPDATE RecordsOfPlants set Plant=?, Photo=? WHERE Plant_id=?",
+                        [update_plant_entry.get().capitalize(), read_file2, dropmenu_var.get()])
+                    
+                    conn.commit()
+            
+            except sqlite3.Error as error:
+                    print("Failed to update data", error)
             
         
         def check_if_plant_exists():
             
             c.execute("SELECT Plant FROM RecordsOfPlants WHERE (Plant=?)",
-                    [update_plant_entry.get()])
+                    [update_plant_entry.get().capitalize()])
 
             result = c.fetchone()
             
@@ -146,7 +180,7 @@ class AddPlant(tk.Frame):
                 clear(update_plant_entry)
             else:
                 messagebox.showerror(
-                    "Error", "The entered plant is not in database")
+                    "Error", "The entered plant is not in database or doesn't match the plant_id")
                 clear(update_plant_entry)
 
 
@@ -163,36 +197,21 @@ class AddPlant(tk.Frame):
                 print("Failed to update data", error)
 
 
-    
-        def delete_data():
-            pass
-        
-
-
-
-        
-        open_button = customtkinter.CTkButton(
-            master=tab1, width=90, text="Choose Image", height=10, compound="left", command=lambda:openFile())
-
-        change_button = customtkinter.CTkButton(master=tab2, width=90, text="Choose Image", height=10, compound="left", command=lambda:openFile())
-        
-        save_image = customtkinter.CTkImage(Image.open(
-            "media\icons8-save-100.png").resize((20, 20), Image.ANTIALIAS))
-
-        save_button = customtkinter.CTkButton(
-            master=tab1, width=90, image=save_image, text="Save",compound="left", command=insert_data)
-
-        home_image = customtkinter.CTkImage(Image.open(
-            "media\icons8-home-256.png").resize((20, 20), Image.ANTIALIAS))
-
-        home_button = customtkinter.CTkButton(
-            master=self, text="", image=home_image, width=80, height=10, command=lambda: controller.show_frame(list_gui.SecondPage))
-
+        change_button = customtkinter.CTkButton(master=tab2, width=90, text="Choose Image", height=10, compound="left", command=lambda:changeFile())     
         update_image = customtkinter.CTkImage(Image.open(
             "media\icons8-update-100.png").resize((20, 20), Image.ANTIALIAS))
 
         update_button = customtkinter.CTkButton(
             master=tab2, text="Update", image=update_image, width=100, height=10, command=update_data)
+    
+
+
+        
+        
+        def delete_data():
+            pass
+        
+
 
         delete = customtkinter.CTkImage(Image.open(
             "media\del.png").resize((20, 20), Image.ANTIALIAS))
@@ -200,10 +219,23 @@ class AddPlant(tk.Frame):
         delete_button = customtkinter.CTkButton(
             master=tab3, text="Delete", fg_color="red2", hover_color="red3", image=delete, width=100, height=10, command=lambda: controller.show_frame(list_gui.SecondPage))
 
+        
 
+
+
+
+        home_image = customtkinter.CTkImage(Image.open(
+            "media\icons8-home-256.png").resize((20, 20), Image.ANTIALIAS))
+
+        home_button = customtkinter.CTkButton(
+            master=self, text="", image=home_image, width=80, height=10, command=lambda: controller.show_frame(list_gui.SecondPage))
+
+
+        
         open_button.place(x=185, y=90)
-        change_button.place(x=185,y=90)
-        update_button.place(x=110, y=170)
+        change_button.place(x=185,y=130)
+        save_button.place(x=100, y=170)
+        update_button.place(x=100, y=170)
         delete_button.place(x=100, y=100)
         home_button.place(x=300, y=400)
-        save_button.place(x=120, y=170)
+        
--- vjezba.py
+++ vjezba.py
@@ -1,81 +1,43 @@
-import tkinter  as tk 
-from tkinter import filedialog
-from tkinter.filedialog import askopenfile
-from PIL import Image, ImageTk
-import io
-import sqlite3
-my_w = tk.Tk()
-my_w.geometry("400x250") 
-my_w.title("www.plus2net.com")  # Adding a title
-from sqlalchemy import create_engine
-from sqlalchemy.exc import SQLAlchemyError
+import tkinter as tk
 
+class CustomText(tk.Text):
+    def __init__(self, *args, **kwargs):
+        """A text widget that supports a <<TextChanged>> event"""
+        tk.Text.__init__(self, *args, **kwargs)
 
+        self.tk.eval('''
+            proc widget_proxy {widget widget_command args} {
 
-conn = sqlite3.connect('another.db')
-c = conn.cursor()
+                # call the real tk widget command with the real args
+                set result [uplevel [linsert $args 0 $widget_command]]
 
-c.execute("CREATE TABLE IF NOT EXISTS student_profile (id,student,profile_photo)")
-try:
-    my_row=c.execute("SELECT student FROM student_profile")
-    my_list = [r for r, in my_row]
-except SQLAlchemyError as e:
-  error=str(e.__dict__['orig'])
-  print(error)    
-options = tk.StringVar(my_w)
-#options.set(my_list[0]) # default value
+                # if the contents changed, generate an event we can bind to
+                if {([lindex $args 0] in {insert replace delete})} {
+                    event generate $widget <<TextModified>> -when tail
+                }
+                # return the result from the real widget command
+                return $result
+            }
 
-om1 =tk.OptionMenu(my_w, options, *my_list)
-om1.grid(row=1,column=1,padx=30)
-global name
-def my_show(*args):  # on select function 
-    global name
-    for i in my_list:
-        if i==options.get():
-            display_data(i)
-            name=i            
-my_font=('times', 12, 'bold')
-b2 = tk.Button(my_w, text='Upload File', 
-   command = lambda:upload_file())
-b2.grid(row=1,column=4) 
-my_font1=('times', 18, 'bold')
-b3 = tk.Button(my_w, text='Update',font=my_font1,
-   command = lambda:update_data())
-b3.grid(row=1,column=5,padx=10) 
+        ''')
 
-global img2
-images = [] # to manage garbage collection. 
-def display_data(name):
-    query="SELECT id,student,profile_photo FROM student_profile WHERE student=?"
-    data=(name,)
-    my_row=c.execute(query,data)
-    student = my_row.fetchone()
-    img = Image.open(io.BytesIO(student[2]))
-    img = ImageTk.PhotoImage(img)
-    
-    l1=tk.Label(my_w,text=student[0])
-    l1.grid(row=1,column=2,ipadx=10)
-    l2=tk.Label(my_w,text=student[1])
-    l2.grid(row=1,column=3)
-    l3 = tk.Label(my_w,image=img )
-    l3.grid(row=3,column=1) 
-    images.append(img) # garbage collection 
-global filename2
-def upload_file(): # Image upload and display
-    global filename2,img2
-    f_types =[('Png files','*.png'),('Jpg Files', '*.jpg')]
-    filename2 = filedialog.askopenfilename(filetypes=f_types)
-    img2 = ImageTk.PhotoImage(file=filename2)
-    b2 =tk.Button(my_w,image=img2) # using Button 
-    b2.grid(row=3,column=2,columnspan=3)#display uploaded photo    
-    images.append(img2) # garbage collection 
-def update_data(): # Update data to MySQL table 
-    global filename2 , name
-    fob=open(filename2,'rb') # filename from upload_file()
-    fob=fob.read()
-    data=(fob,name) # tuple with data 
-    c.execute("UPDATE student_profile set profile_photo=? WHERE student=?",data)
-    
-    my_w.destroy() # close window after adding data    
-options.trace('w',my_show)
-my_w.mainloop()
+        # this replaces the underlying widget with the proxy
+        self.tk.eval('''
+            rename {widget} _{widget}
+            interp alias {{}} ::{widget} {{}} widget_proxy {widget} _{widget}
+        '''.format(widget=str(self)))
+
+def update_char_count(event):
+    count = event.widget.count("1.0", "end-1c")
+    # count is a tuple; the character count is the first element
+    count = 0 if not count else count[0]
+    label.configure(text=f"Characters: {count}")
+
+root = tk.Tk()
+text = CustomText(root)
+label = tk.Label(root, anchor="w")
+label.pack(side="bottom", fill="x")
+text.pack(fill="both", expand=True)
+
+text.bind("<<TextModified>>", update_char_count)
+root.mainloop()
